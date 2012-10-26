6aae55a109c799dc3f8a4f25dee066dc721b577f
adam <tgwizard@gmail.com>
2012-10-26 11:26:16 +0200

    Added code comments

--- thesis/figures/formal_syntax_example_1.py
+++ thesis/figures/formal_syntax_example_1.py
@@ -6,7 +6,10 @@ import fibmodule
 @rv.monitor(func=fibmodule.fib)
 @formal_spec
 def spec():
+  # create assert specification
   a = make_assert(
           lambda event: event.fn.func.inputs[0] > 0)
+  # create next specification
   n = make_next(spec)
+  # combine by composition
   return a + n
--- thesis/figures/formal_syntax_example_2.py
+++ thesis/figures/formal_syntax_example_2.py
@@ -7,19 +7,27 @@ import mymodule
 	bar=mymodule.bar)
 @formal_spec
 def spec():
+  # create assert specification
   a = make_assert(
           lambda event: event.fn.foo.called)
 
+  # create if specification, with guard expression
+  # and an assert specification in the then clause
   if_guard = lambda event: event.fn.foo.inputs[0] == 0
   if_then = make_assert(
           lambda event: event.fn.foo.inputs[1] == 0)
   i = make_if(if_guard, if_then)
 
+  # create next specification
   n = make_next(spec_bar_called)
+  # combine the three by composition
   return a + i + n
 
 def spec_bar_called():
+  # create assert specification
   a = make_assert(
           lambda event: event.fn.bar.called)
+  # create next specification
   n = make_next(spec)
+  # combine by composition
   return a + n
