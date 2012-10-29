77d1c2ddb4365422c1242c0072ed025c0dc2d2f5
adam <tgwizard@gmail.com>
2012-10-29 08:26:16 +0100

    Some fixes

--- thesis/figures/app_authentication_informal.py
+++ thesis/figures/app_authentication_informal.py
@@ -1,21 +1,29 @@
 from pythonrv import rv
+from django.core.handlers.base import BaseHandler
 # disable copying to work around bug in django
 rv.configure(enable_copy_args=False)
 
 # monitor the main request-processing method that
 # has access to both the request and the response
-from django.core.handlers.base import BaseHandler
 @rv.monitor(bh=BaseHandler.get_response)
 def ensure_auth(event):
   request = event.called_function.inputs[1]
   response = event.called_function.result
+  if requires_auth(request, response):
+    assert (request.user.is_authenticated(),
+      "The current user is not authenticated")
+    assert (request.user.is_active,
+      "The current user is not active")
+
+# a helper function for when authentication is
+# required
+def requires_auth(req, res):
   # only ok responses need authentication
-  if response.status_code == 200:
-    # requests to /login does not require auth
-    # /appmedia is only css and images
-    if not (request.path.startswith("/login") or
-        request.path.startswith("/appmedia")):
-        assert (request.user.is_authenticated(),
-          "The current user is not authenticated")
-        assert (request.user.is_active,
-          "The current user is not active")
+  if res.status_code != 200:
+    return False
+  # requests to /login does not require auth nor
+  # does /appmedia, which  is only css and images
+  if (req.path.startswith("/login") or
+      req.path.startswith("/appmedia")):
+    return False
+  return True
